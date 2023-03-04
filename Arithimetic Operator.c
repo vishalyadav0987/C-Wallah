fb51d4a501144e018fa6b3e655adb231619b568c
vishalyadav0987 <viahalyadav0987@gmail.com>
2023-03-04 20:22:39 +0530

    Arithimetic Operator

---
+++ Arithimetic Operator.c
@@ -0,0 +1,34 @@
+// operator in python
+// #include<stdio.h>
+// int main()
+// {
+//         // %d for integer
+//     int a,b;
+//     a=6;
+//     b=5;
+//     // scanf("Enter 1st number%d"," ",&a);
+//     // scanf("Enter 1st number%d"," ",&b);
+//     printf("a+b = %d\n",a+b);
+//     printf("a-b = %d\n",a-b);
+//     printf("a*b = %d\n",a*b);
+//     printf("a/b = %d\n",a/b);
+//     printf("a%b = %d\n",a%b);
+//     return 0;
+// }
+
+#include<stdio.h>
+int main()
+{ 
+    // %f for floating point nunber
+    int a;
+    a=6;
+    float b = 5;
+    // scanf("Enter 1st number%d"," ",&a);
+    // scanf("Enter 1st number%d"," ",&b);
+    printf("a+b = %f\n",a+b);
+    printf("a-b = %f\n",a-b);
+    printf("a*b = %f\n",a*b);
+    printf("a/b = %f\n",a/b);
+    // printf("a%b = %f\n",a%b); not possible
+    return 0;
+}
