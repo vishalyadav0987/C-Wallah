adbe064a35d79f3bb4d4376ab61623720cd7af59
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2023-05-01 18:44:37 +0530

    ASCII

---
+++ ASCII.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter ASCII number : ");  // American standard code for information interchange
+    
+    scanf("%d",&a);
+    printf("ASCII value = %d\nGiven Character = %c",a,a);
+}
