d78bd980212dffce34692ae681cee3085fdeb6d6
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2023-05-12 23:01:46 +0530

    sortingArray

---
+++ sortingArray.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n, t, j;
+    printf("Enter how many no. you input want : ");
+    scanf("%d", &n);
+    int a[n];
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+            }
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
+    }
+}
